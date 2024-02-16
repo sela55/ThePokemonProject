4d5cc9405ad02458589ab6455a3997e1e6f77eda
sela_shabtai <sela@aniview.com>
2024-02-16 17:26:39 +0200

    Initial commit

---
+++ test_pokemon_api.py
@@ -0,0 +1,67 @@
+import json
+
+import pytest
+import requests
+from soft_assert import check, verify
+
+
+class TestPokemonApi:
+    response = None
+    content = None
+    fire_pokemons = {}
+    memory = {}
+
+
+    @classmethod
+    def setup_class(cls):
+        # Setup code here
+        cls.response = requests.get("https://pokeapi.co/api/v2/type")
+        content = cls.response.json()
+        for result in content["results"]:
+            if result["name"] == "fire":
+                link = result["url"]
+                type_response = requests.get(link)
+                type_content = type_response.json()
+                for pokemon in type_content["pokemon"]:
+                    cls.fire_pokemons[pokemon["pokemon"]["name"]] = pokemon["pokemon"]["url"]
+                break
+
+    def test_q1(self):
+        # content = response.content
+        pokemonSetStructure = set()
+        content = self.response.json()
+        # for i in range(len(content["results"])):
+        #     pokemonSetStructure.add(content["results"][i]["name"])
+        for result in content["results"]:
+            pokemonSetStructure.add(result["name"])
+        assert len(pokemonSetStructure) == 20
+
+    @pytest.mark.parametrize("name, expected", [("charmander", True), ("bulbasaur", False)])
+    def test_q2(self, name, expected):
+        assert (name in self.fire_pokemons.keys()) == expected
+
+    @pytest.mark.parametrize("name_fire_pokemon", [("charizard-gmax", 10000), ("cinderace-gmax", 10000),
+                                              ("coalossal-gmax", 10000),
+                                              ("centiskorch-gmax", 10000), ("groudon-primal", 9997)])
+    def test_q3(self, name_fire_pokemon):
+        if "tuple_fire_pokemons_and_weight" not in self.memory.keys():
+            weight_dict_for_fire_pokemons = dict()
+            for name, url in self.fire_pokemons.items():
+                response = requests.get(url)
+                content = response.json()
+                weight_dict_for_fire_pokemons[name] = content["weight"]
+            tuple_fire_pokemons_and_weight = tuple(weight_dict_for_fire_pokemons.items())
+            tuple_fire_pokemons_and_weight = sorted(tuple_fire_pokemons_and_weight, key=lambda x: x[1])
+            tuple_fire_pokemons_and_weight = tuple_fire_pokemons_and_weight[-5:]
+            self.memory["tuple_fire_pokemons_and_weight"] = tuple_fire_pokemons_and_weight
+            # print(tuple_fire_pokemons_and_weight)
+        assert name_fire_pokemon in self.memory["tuple_fire_pokemons_and_weight"]
+
+
+
+
+
+
+
+
+
